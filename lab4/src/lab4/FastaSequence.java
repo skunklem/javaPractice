8e98d62d4641cbdf6a973669e91865101375dd04
root <root@DESKTOP-21QBQVE.localdomain>
2021-10-06 15:27:56 -0400

    added a few comparators to FastaSequence

--- lab4/src/lab4/FastaSequence.java
+++ lab4/src/lab4/FastaSequence.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class FastaSequence
@@ -108,24 +112,97 @@ public class FastaSequence
 		writer.flush(); writer.close();
 	}
 
-	
-
 	public static void main(String[] args) throws Exception
 	{
 		// parse fasta file and return list of FastaSequence objects
 		List<FastaSequence> fastaList = FastaSequence.readFastaFile("test.fasta");
+	    
+	    // implement comparable - alphabetically by header
+	    Comparator<FastaSequence> headerABC = new Comparator<FastaSequence>()
+		{
+			public int compare(FastaSequence f1,FastaSequence f2)
+			{
+				return f1.getHeader().compareTo(f2.getHeader());
+			}
+		};
+
+	    // implement comparable - alphabetically by sequence
+	    Comparator<FastaSequence> seqABC = new Comparator<FastaSequence>()
+		{
+			public int compare(FastaSequence f1,FastaSequence f2)
+			{
+				return f1.getSequence().compareTo(f2.getSequence());
+			}
+		};
+
+	    // implement comparable - numerically by GC content
+	    Comparator<FastaSequence> gc123 = new Comparator<FastaSequence>()
+		{
+			public int compare(FastaSequence f1,FastaSequence f2)
+			{
+				return Float.compare(f1.getGCRatio(), f2.getGCRatio());
+			}
+		};
 		
-		// print info about seqs
-	    for( FastaSequence fs : fastaList)
-	    {
-	    	System.out.println(fs.getHeader());
-	        System.out.println(fs.getSequence());
-	        System.out.println(fs.getGCRatio());
-	    }
+	    // implement comparable - numerically by number of valid characters
+	    Comparator<FastaSequence> validChars = new Comparator<FastaSequence>()
+		{
+			public int compare(FastaSequence f1,FastaSequence f2)
+			{
+				int[] values = {0,0};
+				String[] seqs = {f1.getSequence(),f2.getSequence()};
+				final HashSet<Character> valid = new LinkedHashSet<Character>();
+				valid.add('A');
+				valid.add('C');
+				valid.add('G');
+				valid.add('T');
+				
+				for ( int i=0; i<seqs.length; i++)
+				{
+					String seq = seqs[i];
+					for ( int x=0;x<seqs[i].length();x++)
+					{
+						if (valid.contains(seq.charAt(x)))
+							values[i]+=1;
+					}
+				}
+				return Integer.compare(values[0],values[1]);
+			}
+		};		
+		
+		// create list of comparators to try out
+		List<Comparator> comps = new ArrayList<Comparator>();
+		List<String> names = new ArrayList<String>();
+		comps.add(headerABC);
+		comps.add(seqABC);
+		comps.add(gc123);
+		comps.add(validChars);
+		names.add("headerABC");
+		names.add("seqABC");
+		names.add("gc123");
+		names.add("validChars");
+		
+		// try out all comparators
+		for ( int x=0; x<comps.size(); x++ )
+		{
+			System.out.println("Comparator: " + names.get(x));
+			Collections.sort(fastaList,comps.get(x));
+			// print info about seqs
+		    for( FastaSequence fs : fastaList)
+		    {
+		    	System.out.println(fs.getHeader());
+		        System.out.println(fs.getSequence());
+		        System.out.println(fs.getGCRatio());
+		    }
+		}
+		
+		
+
 	
 	    // write table file with more info
 	    File myFile = new File("fastaTableOut.txt");
 	    writeTableSummary( fastaList,  myFile);
+
 	}
 
 
